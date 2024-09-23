f3370e86674e7b7b2d6de7d9765db9e72e2ee7b5
Kira <kira.doniy@gmail.com>
2024-09-23 08:19:38 +0300

    Initial commit

---
+++ src/main/java/com/neoflex/edu/CalculatorHolidayPay/CalculatorHolidayPayApplication.java
@@ -0,0 +1,13 @@
+package com.neoflex.edu.CalculatorHolidayPay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorHolidayPayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorHolidayPayApplication.class, args);
+	}
+
+}
